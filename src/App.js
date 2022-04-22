d6ef86c399af970217e5f02b63332eb32c22008d
Calzar1001 <85040148+Calzar1001@users.noreply.github.com>
2022-04-22 12:41:51 -0600

    Finalized draft of lab 10
    Technology summary complete
    Formatted App.js Layout.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
 
+import Header from './Header.js';
+import Education from './Education';
+import TechnologySummary from './TechnologySummary';
+import WorkExperience from './WorkExperience';
+import Projects from './Projects';
+
 function App() {
   return (
     <div className="App">
-      <h1> Hello world </h1>
+      <Header />
+
+      <main className="container">
+        <div className="row mt-3">
+
+          <div className="col-md">
+            <WorkExperience />
+          </div>
+
+          <div className="col-md">
+            <TechnologySummary />
+          </div>
+
+
+        </div>
+        <div className="row mt-3">
+
+          <div className="col-md">
+            <Projects />
+          </div>
+
+          <div className="col-md">
+            <Education />
+          </div>
+
+        </div>
+      </main>
     </div>
   );
 }
